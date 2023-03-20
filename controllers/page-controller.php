a637688b35945e82137e74a871ed0e732af8d7c1
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2023-03-20 13:30:13 +0100

    Gebruik Exceptions niet als je je de exception zelf gooit in de try erboven #16

--- controllers/page-controller.php
+++ controllers/page-controller.php
@@ -131,24 +131,17 @@ class PageController
 
     private function showResponsePage()
     {
-        try {
-            $this->model->createMenuArr();
-            $current_page = $this->model->page;
-            if (!@include_once("views/{$current_page}_doc.php")) // @ - to suppress warnings                
-            {
-                throw new Exception("views/{$current_page}_doc.php does not exist");
-            }
-            require_once("views/{$current_page}_doc.php");
+        $this->model->createMenuArr();
+        $current_page = $this->model->page;
+        if (@include_once("views/{$current_page}_doc.php")) { // @ - to suppress warnings             
             $class = "{$current_page}Doc";
             $view = new $class($this->model);
-            $view->show($current_page);
-        } catch (Exception $e) {
-            echo '<a href="index.php?page=home">Go to homepage</a>';
-            debug_to_console($e->getMessage());
-            require_once("views/html_doc.php");
-            $view = new HtmlDoc($this->model);
-            $view->show();
+        } else {
+            debug_to_console("unknown page: " . $current_page);
+            require_once("views/home_doc.php");
+            $view = new HomeDoc($this->model);
         }
+        $view->show();
     }
 }
 
