faf6cc9ae01ba083f12cd2d7cb3abb6958317e02
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2023-04-06 16:36:57 +0200

    update DS-opdracht-6.1.4

--- Database/DS-opdracht-6.1.4.sql
+++ Database/DS-opdracht-6.1.4.sql
@@ -1,16 +1,15 @@
-SELECT if(isnull(r1.name), r2.name, concat_ws(' - ', r1.name, r2.name)) name, N.numsupp, N.hc
-FROM mhl_rubrieken r1
-RIGHT JOIN mhl_rubrieken r2
-ON r2.parent=r1.id
-RIGHT JOIN (
-SELECT r.id, IFNULL(COUNT(s.id), 0) numsupp, SUM(hitcount) hc from mhl_rubrieken r
-JOIN mhl_suppliers_mhl_rubriek_view rv
+SELECT concat_ws(' - ', r1.name, r2.name) name, H.hit
+FROM mhl_rubrieken r2
+LEFT JOIN mhl_rubrieken r1
+ON r1.id=r2.parent
+LEFT JOIN (
+SELECT r.id, r.name, IFNULL(SUM(hitcount), 'Geen hits') hit
+FROM mhl_suppliers_mhl_rubriek_view rv
+LEFT JOIN mhl_hitcount hc
+ON hc.supplier_ID = rv.mhl_suppliers_ID
+RIGHT JOIN mhl_rubrieken r
 ON r.id = rv.mhl_rubriek_view_ID
-JOIN mhl_suppliers s
-ON rv.mhl_suppliers_ID = s.id
-JOIN mhl_hitcount h
-ON s.id = h.supplier_ID
 GROUP BY r.id
-) N
-on N.id = r2.id
+) H
+ON H.id = r2.id
 ORDER BY name
